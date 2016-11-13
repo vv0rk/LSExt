04d1053dd04548a0f93664ad2c3f4ae43a022145
Nikolay Savin <savinnv1970@gmail.com>
2016-11-13 20:54:43 +0800

    Add project files.

---
+++ LSExt/Controllers/OrdersController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using LSExt.Models;
+using DevExtreme.AspNet.Data;
+
+namespace LSExt.Controllers
+{
+	public class OrdersController : ApiController
+	{
+
+		[HttpGet]
+		public HttpResponseMessage Get(DataSourceLoadOptions loadOptions)
+		{
+			return Request.CreateResponse(DataSourceLoader.Load(SampleData.Orders, loadOptions));
+		}
+
+	}
+}
---
+++ LSExt/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LSExt.MvcApplication" Language="C#" %>
